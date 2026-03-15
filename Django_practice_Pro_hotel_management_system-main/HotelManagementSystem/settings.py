b44b20d2a3ad1671432aa966d5e4c0b39ce6dfcd
Muhammed  Abdulhadi <mrglasswillbreak@gmail.com>
2026-03-15 21:28:13 +0000

    Use Django 4.2+ STORAGES dict instead of deprecated STATICFILES_STORAGE

--- Django_practice_Pro_hotel_management_system-main/HotelManagementSystem/settings.py
+++ Django_practice_Pro_hotel_management_system-main/HotelManagementSystem/settings.py
@@ -137,7 +137,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
 if not DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STORAGES = {
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
